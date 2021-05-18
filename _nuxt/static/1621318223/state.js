ac0432268c83dafbee20ffc27fdb15fc83f23678
danushka96 <danushkaherath96@gmail.com>
2021-05-18 11:43:04 +0530

    db90327

---
+++ _nuxt/static/1621318223/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-web\u002F_nuxt\u002Fstatic\u002F1621318223",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fnuxt-web\u002F",assetsPath:"\u002Fnuxt-web\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3426e17e"}}}}(null));
